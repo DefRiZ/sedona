2b725fd37d46b1533cd868c42e3ed737ad6c1c02
DefRiZ <90318975+DefRiZ@users.noreply.github.com>
2023-09-19 22:58:30 +0300

    Header was completed

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Header.module.scss";
+import global from "../../global.module.scss";
+
+import logo from "../../img/header/logo.svg";
+import search from "../../img/header/search.svg";
+import favorites from "../../img/header/heart.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.container}>
+        <nav>
+          <ul>
+            <li>
+              <Link className={styles.link} to="/">
+                Главная
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} to="/">
+                О Седоне
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} to="/">
+                Гостиницы
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.logo}>
+          <Link to="/">
+            <img src={logo} alt="logotype" />
+          </Link>
+        </div>
+        <div className={styles.secondNav}>
+          <Link className={styles.search} to="/">
+            <img src={search} alt="searching" />
+          </Link>
+          <button className={styles.fav}>
+            <img src={favorites} alt="favorites places" />
+            <span>12</span>
+          </button>
+          <Link className={`${styles.btn}, ${global.btn}`} to="/">
+            Хочу сюда!
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        containerFull: "1200px",
+      },
+      colors: {
+        favorite: "#7DB54F",
+        accent: "#756257",
+      },
+      fontSize: {
+        fav: "1.0rem",
+        link: "2.0rem",
+        btn: "1.6rem",
+      },
+    },
   },
   plugins: [],
 };
