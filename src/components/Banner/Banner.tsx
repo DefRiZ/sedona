b9106b8bd47eb8f64b22ab355dff8268f8135d45
DefRiZ <90318975+DefRiZ@users.noreply.github.com>
2023-09-22 11:33:02 +0300

    Banner has been finished

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./Banner.module.scss";
+
+import background from "../../img/banner/background.jpg";
+import title from "../../img/banner/welcome.svg";
+import decorate from "../../img/banner/decorate.svg";
+
+const Banner = () => {
+  return (
+    <section>
+      <div className={styles.root}>
+        <img className={styles.title} src={title} alt="title" />
+        <img className={styles.banner} src={background} alt="banner" />
+        <img className={styles.decorate} src={decorate} alt="decorate" />
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
